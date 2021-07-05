9c86f0146a0d880d0a2ba37a0a6c86ca95f27371
Philipp Markus Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2021-07-05 10:45:07 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ndspectral
 
-## ndspectral: *n*-dimensional transforms of various basis functions (spectral method)
+## ndspectral: *n*-dimensional transforms of various basis functions
+
+This library is intended for simulation softwares which solve the
+partial differential equations using spectral methods.
+
+Currently ndspectral implements transforms from physical to spectral space
+for the following basis functions:
+- Chebyshev (Orthonormal)
+- ChebDirichlet (Composite)
+- ChebNeumann (Composite)
+
+Composite basis combine several basis functions of its parent space to
+satisfy the needed boundary conditions, this is often called a Galerkin method.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 #![warn(missing_docs)]
 #![warn(missing_doc_code_examples)]
-//! # ndspectral: *n*-dimensional transforms of various basis functions (spectral method)
+//! # ndspectral: *n*-dimensional transforms of various basis functions
+//!
+//! This library is intended for simulation softwares which solve the
+//! partial differential equations using spectral methods.
+//!
+//! Currently ndspectral implements transforms from physical to spectral space
+//! for the following basis functions:
+//! - Chebyshev (Orthonormal)
+//! - ChebDirichlet (Composite)
+//! - ChebNeumann (Composite)
+//!
+//! Composite basis combine several basis functions of its parent space to
+//! satisfy the needed boundary conditions, this is often called a Galerkin method.
 pub mod bases;
 pub use bases::{ChebDirichlet, ChebNeumann, Chebyshev};
 
