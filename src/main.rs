af310c938f04d057df0a1f1cf739f824357060f3
Philipp Markus Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2021-07-10 13:55:08 +0200

    v0.1.0

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     let ra = 1e5;
     let pr = 1.;
     let adiabatic = true;
-    let aspect = 4.0;
+    let aspect = 1.0;
     let dt = 0.01;
     let mut navier = Navier2D::new(nx, ny, ra, pr, dt, adiabatic, aspect);
-    navier.read("data/flow40.000.h5");
+    //navier.read("data/flow40.000.h5");
     integrate(navier, 100.0, Some(1.0));
 }
