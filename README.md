6552d501758f7742b9f22d24c7b50608d2153c05
preiter <preiter@gwdg.de>
2021-07-23 10:56:28 +0200

    added: create_xmf

--- README.md
+++ README.md
@@ -79,7 +79,7 @@ by the script
 
 `./bin/create_xmf`.
 
-This script works only for fields from the `Navier2D'
+This script works only for fields from the `Navier2D`
 solver with the attributes temp, ux, uy and pres.
 The bin folder contains also the full crate `create_xmf`, which
 can be adapted for specific usecases.
--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,7 @@
 //!
 //! `./bin/create_xmf`.
 //!
-//! This script works only for fields from the `Navier2D'
+//! This script works only for fields from the `Navier2D`
 //! solver with the attributes temp, ux, uy and pres.
 //! The bin folder contains also the full crate `create_xmf`, which
 //! can be adapted for specific usecases.
