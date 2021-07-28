d286b07b6c19442ce2f1ad0d0735b21e9ba6cc4a
preiter <preiter@gwdg.de>
2021-07-28 10:42:38 +0200

    update

--- src/hdf5.rs
+++ src/hdf5.rs
@@ -130,6 +130,36 @@ pub fn write_to_hdf5(
     Ok(())
 }
 
+/// Retrieve size of dimenion from an hdf5 file
+pub fn hdf5_get_size_dimension<P: AsRef<Path>>(filename: P, name: &str) -> hdf5::Result<usize> {
+    let file = hdf5::File::open(filename)?;
+    let dset = file.dataset(name)?;
+
+    assert!(
+        dset.shape().len() == 1,
+        "Dimension must be of size 1, but is of size {}",
+        dset.shape().len()
+    );
+
+    Ok(dset.shape()[0])
+}
+
+/// Retrieve size of dimenion from an hdf5 file
+pub fn hdf5_get_scalar<P: AsRef<Path>>(filename: P, name: &str) -> hdf5::Result<f64> {
+    let file = hdf5::File::open(filename)?;
+    let dset = file.dataset(name)?;
+
+    assert!(
+        dset.shape().len() == 1,
+        "Dimension must be of size 1, but is of size {}",
+        dset.shape().len()
+    );
+
+    let scalar: Array1<f64> = dset.read()?;
+
+    Ok(scalar[0])
+}
+
 /// Generate full variable path inside hdf5 file from name
 /// of the variable and name of the group (optional)
 fn gen_name_path(name: &str, group: Option<&str>) -> String {
