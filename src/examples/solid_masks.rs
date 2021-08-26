2573a109c8c10fe532ba7badeef454cef8a49085
Philipp Markus Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2021-08-26 17:20:44 +0200

    added porosity + interpolation

--- src/examples/solid_masks.rs
+++ src/examples/solid_masks.rs
@@ -55,44 +55,6 @@ pub fn solid_cylinder_inner(
     [mask, value]
 }
 
-/// Return multiple circles to mimik porosity
-#[allow(clippy::cast_possible_truncation)]
-pub fn solid_porosity(
-    x: &Array1<f64>,
-    y: &Array1<f64>,
-    diameter: f64,
-    porosity: f64,
-) -> [Array2<f64>; 2] {
-    let mut mask = Array2::<f64>::zeros((x.len(), y.len()));
-    let value = Array2::<f64>::zeros(mask.raw_dim());
-    let radius = diameter / 2.;
-    let length = x[x.len() - 1] - x[0];
-    let height = y[y.len() - 1] - y[0];
-    // Number of elements
-    let num_circles_in_x = ((1. - porosity) * 4. * length.powi(2) / (PI * diameter.powi(2)))
-        .sqrt()
-        .round();
-    let num_circles_in_y = ((1. - porosity) * 4. * height.powi(2) / (PI * diameter.powi(2)))
-        .sqrt()
-        .round();
-    // Distance between elements
-    let distance_x = (length - num_circles_in_x * diameter) / (num_circles_in_x + 1.);
-    let distance_y = (height - num_circles_in_y * diameter) / (num_circles_in_x + 1.);
-    // Construct mask
-    let mut origin_x = x[0] + distance_x + radius;
-    for _i in 0..num_circles_in_x as usize {
-        let mut origin_y = y[0] + distance_y + radius;
-        for _j in 0..num_circles_in_y as usize {
-            let mask_circle = solid_cylinder_inner(x, y, origin_x, origin_y, radius);
-            mask += &mask_circle[0];
-            origin_y += distance_y + diameter;
-            // println!("ox: {:?} oy: {:?}", origin_x, origin_y);
-        }
-        origin_x += distance_x + diameter;
-    }
-    [mask, value]
-}
-
 /// Return mask for sinusoidal roughness elements
 pub fn solid_roughness_sinusoid(
     x: &Array1<f64>,
@@ -131,4 +93,83 @@ pub fn solid_roughness_sinusoid(
         }
     }
     [mask, value]
+}
+
+/// Return multiple circles which mimik porosity
+#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+pub fn solid_porosity(
+    x: &Array1<f64>,
+    y: &Array1<f64>,
+    diameter: f64,
+    porosity: f64,
+) -> [Array2<f64>; 2] {
+    let mut mask = Array2::<f64>::zeros((x.len(), y.len()));
+    let value = Array2::<f64>::zeros(mask.raw_dim());
+    let radius = diameter / 2.;
+    let length = x[x.len() - 1] - x[0];
+    let height = y[y.len() - 1] - y[0];
+    // Number of elements
+    let num_circles_in_x = ((1. - porosity) * 4. * length.powi(2) / (PI * diameter.powi(2)))
+        .sqrt()
+        .round();
+    let num_circles_in_y = ((1. - porosity) * 4. * height.powi(2) / (PI * diameter.powi(2)))
+        .sqrt()
+        .round();
+    // Distance between elements
+    let distance_x = (length - num_circles_in_x * diameter) / (num_circles_in_x + 1.);
+    let distance_y = (height - num_circles_in_y * diameter) / (num_circles_in_x + 1.);
+    // Construct mask
+    let mut origin_x = x[0] + distance_x + radius;
+    for _i in 0..num_circles_in_x as usize {
+        let mut origin_y = y[0] + distance_y + radius;
+        for _j in 0..num_circles_in_y as usize {
+            let mask_circle = solid_cylinder_inner(x, y, origin_x, origin_y, radius);
+            mask += &mask_circle[0];
+            origin_y += distance_y + diameter;
+            // println!("ox: {:?} oy: {:?}", origin_x, origin_y);
+        }
+        origin_x += distance_x + diameter;
+    }
+    [mask, value]
+}
+
+/// Interpolates porosity from a 513 x 513 grid onto the requested size
+/// for base chebyshev / chebyshev.
+pub fn solid_porosity_interpolate(
+    nx: usize,
+    ny: usize,
+    diameter: f64,
+    porosity: f64,
+) -> [Array2<f64>; 2] {
+    use crate::{chebyshev, Field2, Space2};
+    let (n, m) = (513, 513);
+    let mut field = Field2::new(&Space2::new(&chebyshev(n), &chebyshev(m)));
+    let mask = solid_porosity(&field.x[0], &field.x[1], diameter, porosity);
+    let mut field_return = Field2::new(&Space2::new(&chebyshev(nx), &chebyshev(ny)));
+    // Mask
+    field.v.assign(&mask[0]);
+    field.forward();
+    broadcast_2d(&field.vhat, &mut field_return.vhat);
+    field_return.backward();
+    let mask_0 = field_return.v.to_owned();
+    // Value
+    field.v.assign(&mask[1]);
+    field.forward();
+    broadcast_2d(&field.vhat, &mut field_return.vhat);
+    field_return.backward();
+    let mask_1 = field_return.v.to_owned();
+    [mask_0, mask_1]
+}
+
+/// Broadcast 2d array
+fn broadcast_2d<T: Clone>(old: &Array2<T>, new: &mut Array2<T>) {
+    use ndarray::s;
+    let sh: Vec<usize> = old
+        .shape()
+        .iter()
+        .zip(new.shape().iter())
+        .map(|(i, j)| *std::cmp::min(i, j))
+        .collect();
+    new.slice_mut(s![..sh[0], ..sh[1]])
+        .assign(&old.slice(s![..sh[0], ..sh[1]]));
 }
