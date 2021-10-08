8f335e6fde3ca90e358769838b0417db64224814
Philipp Markus Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2021-10-08 14:03:02 +0200

    extended particle tracing

--- bin/particle_tracer/src/main.rs
+++ bin/particle_tracer/src/main.rs
@@ -1,46 +1,9 @@
 use hdf5_interface::read_from_hdf5;
 use ndarray::prelude::*;
 use particle_tracer::Particle;
+use rand::{self, Rng};
 use std::path::Path;
 use std::path::PathBuf;
-
-fn main() {
-    let (nx, ny) = (51, 51);
-    let x = Array1::linspace(-1., 1., nx);
-    let y = Array1::linspace(-1., 1., nx);
-    let mut ux: Array2<f64> = Array2::zeros((nx, ny));
-    let mut uy: Array2<f64> = Array2::zeros((nx, ny));
-
-    // Circular veclocity field
-    for (i, xi) in x.iter().enumerate() {
-        for (j, yi) in y.iter().enumerate() {
-            ux[[i, j]] = -yi;
-            uy[[i, j]] = *xi;
-        }
-    }
-
-    let mut particle = Particle::init(0.2, 0., x.as_slice().unwrap(), y.as_slice().unwrap());
-    particle.set_intervall(0.1);
-    particle.set_timestep(0.1);
-    loop {
-        particle.update(&[&ux.view(), &uy.view()]).unwrap();
-        if particle.time > 10.0 {
-            break;
-        }
-    }
-    particle.write("test_trajectory.txt").unwrap();
-
-    let mut particle = Particle::init(0.2, 0., x.as_slice().unwrap(), y.as_slice().unwrap());
-    particle.set_intervall(0.1);
-    particle.set_timestep(0.1);
-    loop {
-        particle.update_rk2(&[&ux.view(), &uy.view()]).unwrap();
-        if particle.time > 10.0 {
-            break;
-        }
-    }
-    particle.write("test_trajectory2.txt").unwrap();
-}
 
 /// Return list of files in root with specific ending
 fn list_of_files_of_type<P: AsRef<Path>>(root: P, ending: &str) -> Vec<PathBuf> {
@@ -53,51 +16,60 @@ fn list_of_files_of_type<P: AsRef<Path>>(root: P, ending: &str) -> Vec<PathBuf> 
         .collect()
 }
 
+fn particle_swarm(x0: f64, y0: f64, range: f64, n: usize) -> Vec<(f64, f64)> {
+    let mut rng = rand::thread_rng();
+    let mut swarm: Vec<(f64, f64)> = vec![];
+    for _ in 0..n {
+        let x = x0 + rng.gen_range(-range..range);
+        let y = y0 + rng.gen_range(-range..range);
+        swarm.push((x, y))
+    }
+    swarm
+}
+
 #[allow(dead_code)]
-fn loop_through_files() {
+fn main() {
     // List files
-    let files = list_of_files_of_type("roll1/", "h5");
-    let (x0, y0) = (0.95, 0.0);
+    let mut files = list_of_files_of_type("data/", "h5");
+    files.sort();
+    //files.sort_by_key(|dir| &dir.path());
+    let x0 = 0.7;
+    let y0 = -0.7;
+    let dt = 0.1;
+    let swarm = particle_swarm(x0, y0, 0.25, 5000);
+    let mut particles: Vec<Particle> = vec![];
+    // init
+    let fname = &files[0].as_path().display().to_string();
+    let x: Array1<f64> = read_from_hdf5(&fname, "x", None).unwrap();
+    let y: Array1<f64> = read_from_hdf5(&fname, "y", None).unwrap();
+    for pos in swarm {
+        let mut particle =
+            Particle::init(pos.0, pos.1, x.as_slice().unwrap(), y.as_slice().unwrap());
+        // particle.set_intervall(None);
+        particle.set_timestep(0.001);
+        particles.push(particle);
+    }
+    // iterate over flow fields
     for file in files {
         let fname = &file.into_os_string().into_string().unwrap();
         println!("Process {:?} ...", fname);
         let ux: Array2<f64> = read_from_hdf5(&fname, "v", Some("ux")).unwrap();
         let uy: Array2<f64> = read_from_hdf5(&fname, "v", Some("uy")).unwrap();
-        let x: Array1<f64> = read_from_hdf5(&fname, "x", None).unwrap();
-        let y: Array1<f64> = read_from_hdf5(&fname, "y", None).unwrap();
-        let mut particle = Particle::init(x0, y0, x.as_slice().unwrap(), y.as_slice().unwrap());
-        particle.set_intervall(0.5);
-        particle.set_timestep(0.001);
-        particle.push();
-        loop {
-            particle.update_rk4(&[&ux.view(), &uy.view()]).unwrap();
-            if particle.time > 1000. {
-                break;
+        // iterate over particles
+        for particle in &mut particles {
+            let t_end = particle.time + dt;
+            loop {
+                particle.update_rk4(&[&ux.view(), &uy.view()]).unwrap();
+                if particle.time >= t_end {
+                    break;
+                }
             }
-        }
-        particle.write(&fname.replace(".h5", "_trajectory.txt")).unwrap();
-    }
-}
-
-
-#[allow(dead_code)]
-fn single_file() {
-    println!("Hello, world!");
-    let fname = "data/test.h5";
-    let ux: Array2<f64> = read_from_hdf5(&fname, "v", Some("ux")).unwrap();
-    let uy: Array2<f64> = read_from_hdf5(&fname, "v", Some("uy")).unwrap();
-    let x: Array1<f64> = read_from_hdf5(&fname, "x", None).unwrap();
-    let y: Array1<f64> = read_from_hdf5(&fname, "y", None).unwrap();
-
-    let (x0, y0) = (0.0, 0.001);
-    let mut particle = Particle::init(x0, y0, x.as_slice().unwrap(), y.as_slice().unwrap());
-    particle.set_intervall(0.5);
-    particle.set_timestep(0.0001);
-    loop {
-        particle.update_rk4(&[&ux.view(), &uy.view()]).unwrap();
-        if particle.time > 1000. {
-            break;
+            particle.history.clear();
+            particle.push();
+            particle
+                .write(&fname.replace(".h5", "_trajectory.txt"))
+                .unwrap();
+            particle.history.clear();
         }
     }
-    particle.write(&fname.replace(".h5", "_trajectory.txt")).unwrap();
 }
---
+++ python/plot_anim2d_particle.py
@@ -0,0 +1,105 @@
+import numpy as np
+import h5py
+import glob
+import re
+import matplotlib.pyplot as plt
+from utils.plot_utils import plot_quiver
+from utils.plot_utils import plot_contour
+import os.path
+import ffmpeg
+
+settings = {
+    "duration": None,  # time in seconds; determines fps
+    "filename": "data/out.mp4",
+}
+
+# -- Get list of files
+fname, time = [], []
+for file in [*glob.glob("*.h5"), *glob.glob("data/*.h5")]:
+    try:
+        time.append(float(re.findall("\d+\.\d+", file)[0]))
+        fname.append(file)
+    except:
+        print("No number found in {:}".format(file))
+idx = np.argsort(time)
+fname = np.array(fname)[idx]
+time = np.array(time)[idx]
+
+for i, f in enumerate(fname):
+    print("# {:3d}: {:}".format(i, f))
+print("From number:")
+i0 = int(input())
+print("To number:")
+i9 = int(input())
+print("Step:")
+step = int(input())
+
+for i, f in enumerate(fname[i0:i9:step]):
+    # -- Read hd5 file
+    filename = f
+    figname = f.replace(".h5", ".png")
+    ptcname = f.replace(".h5", "_trajectory.txt")
+
+    if os.path.isfile(figname):
+        print("{} already exists".format(figname))
+        continue
+
+    with h5py.File(filename, "r") as f:
+        t = np.array(f["temp/v"])
+        u = np.array(f["ux/v"])
+        v = np.array(f["uy/v"])
+        x = np.array(f["x"])
+        y = np.array(f["y"])
+
+    # read particle position
+    particle = np.loadtxt(ptcname)
+    pos_x = particle[:, 1]
+    pos_y = particle[:, 2]
+    print("Plot {:}".format(filename))
+    fig, ax = plot_contour(x, y, t, u, v, return_fig=True)
+    ax.scatter(pos_x, pos_y, c="k", s=3, alpha=0.5)
+    fig.savefig(figname, bbox_inches="tight", dpi=140)
+    plt.close("all")
+
+# -- Get list of pngs
+fname, time = [], []
+for file in [*glob.glob("data/*.png")]:
+    try:
+        time.append(float(re.findall("\d+\.\d+", file)[0]))
+        fname.append(file)
+    except:
+        print("No number found in {:}".format(file))
+idx = np.argsort(time)
+files = list(np.array(fname)[idx])
+time = np.array(time)[idx]
+
+for i, f in enumerate(files):
+    print("# {:3d}: {:}".format(i, f))
+
+if settings["duration"] is None:
+    print("How long should the movie be? (seconds)")
+    settings["duration"] = float(input())
+
+n_frames = len(files)
+fps = n_frames / settings["duration"]
+
+# Execute FFmpeg sub-process, with stdin pipe as input, and jpeg_pipe input format
+process = (
+    ffmpeg.input("pipe:", r=fps, f="png_pipe")
+    .output(settings["filename"], vcodec="libx264", pix_fmt="yuv420p")
+    .overwrite_output()
+    .run_async(pipe_stdin=True)
+)
+
+# Iterate jpeg_files, read the content of each file and write it to stdin
+for in_file in files:
+    with open(in_file, "rb") as f:
+        # Read the JPEG file content to jpeg_data (bytes array)
+        data = f.read()
+
+        # Write JPEG data to stdin pipe of FFmpeg process
+        process.stdin.write(data)
+
+# Close stdin pipe - FFmpeg fininsh encoding the output file.
+process.stdin.close()
+process.wait()
